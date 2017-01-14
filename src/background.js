02c0dfdf2b9a7645536d1e91694fd0fe429b79d7
tyru <tyru.exe@gmail.com>
2017-01-14 20:50:24 +0900

    Promisify

--- src/background.js
+++ src/background.js
@@ -1,56 +1,70 @@
 chrome.commands.onCommand.addListener(function(command) {
 	console.log('Command:', command);
+
+	function promisify(fn) {
+		return function(){
+			let args = Array.prototype.slice.call(arguments);
+			return new Promise(function(resolve){
+				fn.apply(this, args.concat(resolve));
+			});
+		}
+	}
+
+	let queryTabs = promisify(chrome.tabs.query);
+	let getAllWindows = promisify(chrome.windows.getAll);
+	let updateWindows = promisify(chrome.windows.update);
+	let updateTabs = promisify(chrome.tabs.update);
+	let moveTabs = promisify(chrome.tabs.move);
+
+	function moveTabsToWindow(tabs, windowId) {
+		if (tabs.length == 0) {
+			return;
+		}
+		moveTabs(tabs.map((e) => e.id), {
+			windowId: windowId,
+			index: -1
+		}).then(function(){
+			console.log('tabs.move');
+			return updateWindows(windowId, {
+				focused: true
+			});
+		}).then(function(){
+			console.log('window.update');
+			return updateTabs(tabs.slice(-1)[0].id, {
+				active: true
+			});
+		}).then(function(){
+			console.log('tabs.update id=' + tabs.slice(-1)[0].id);
+			return queryTabs({
+				currentWindow: true
+			});
+		})
+	}
 	
 	switch (command) {
 	case "move_tab":
-		let queryInfo = {
+		queryTabs({
 			active: true,
 			currentWindow: true
-		};
-		chrome.tabs.query(queryInfo, function(tabs){
+		}).then(function(tabs){
 			console.log('tabs.query', tabs);
-			chrome.windows.getAll(function(windows){
+			return getAllWindows().then(function(windows){
 				console.log('window.getAll', windows);
-				
-				let destWindow = null;
-				for (let i=0; i < windows.length; i++) {
-					if (!windows[i].focused) {
-						destWindow = windows[i];
-						break;
-					}
-				}
-				if (destWindow) {
-					let moveProperties = {
-						windowId: destWindow.id,
-						index: -1
-					};
-					chrome.tabs.move(tabs.map((e) => e.id), moveProperties, function(){
-						console.log('tabs.move');
-						let updateInfo = {
-							focused: true
-						};
-						chrome.windows.update(destWindow.id, updateInfo, function(){
-							console.log('window.update');
-							let updateProperties = {
-								active: true
-							};
-							chrome.tabs.update(tabs.slice(-1)[0].id, updateProperties, function(){
-								console.log('tabs.update id=' + tabs.slice(-1)[0].id);
-								
-								let queryInfo = {
-									currentWindow: true
-								};
-								chrome.tabs.query(queryInfo, function(tabs){
-									console.log('tabs.query', tabs);
-									
-									
-								});
-							});
-						});
-					});
-				}
+				return [windows, tabs];
 			});
-		});
+		}).then(function (wintabs) {
+			let windows = wintabs[0], tabs = wintabs[1];
+			let destWindow = null;
+			for (let i=0; i < windows.length; i++) {
+				if (!windows[i].focused) {
+					destWindow = windows[i];
+					break;
+				}
+			}
+			if (destWindow) {
+				moveTabsToWindow(tabs, destWindow.id);
+			}
+		})
 		break;
 	default:
 		break;
